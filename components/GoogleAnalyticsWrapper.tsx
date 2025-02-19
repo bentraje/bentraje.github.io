e45de68c42a4352f64a9100728430ecb4a793f43
Ben Traje <52109229+bentraje@users.noreply.github.com>
2025-02-19 21:34:54 +0800

    please mag work ka na huhuhu

--- components/GoogleAnalyticsWrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client';  // Ensures this is client-side only
-
-import { GoogleAnalytics } from 'nextjs-google-analytics';
-
-export function GoogleAnalyticsWrapper() {
-  return <GoogleAnalytics trackPageViews />;
-}
